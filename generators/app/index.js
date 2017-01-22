640ac71ca23b1e4f28cde2392a2f2630445e071e
Mohamed Turki <hi@mturki.me>
2017-01-22 16:03:18 +0100

    Adds a snippet template

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,19 +2,28 @@
 var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var _ = require('lodash');
 
 module.exports = Generator.extend({
   prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the incredible ' + chalk.red('generator-sublime-snippets') + ' generator!'
-    ));
-
     var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'title',
+      message: 'How would you like to name this snippet?',
+      default: 'snippet'
+    }, {
+      type: 'input',
+      name: 'content',
+      message: 'What\'s the content of this snippet?'
+    }, {
+      type: 'input',
+      name: 'trigger',
+      message: 'How to trigger this snippet?'
+    }, {
+      type: 'input',
+      name: 'scope',
+      message: 'What\'s the scope (file extensition) to run this snippet in?',
+      default: 'js'
     }];
 
     return this.prompt(prompts).then(function (props) {
@@ -24,13 +33,15 @@ module.exports = Generator.extend({
   },
 
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      this.templatePath('snippet.sublime-snippet'),
+      this.destinationPath(`${_.kebabCase(this.props.title)}.sublime-snippet`),
+      {
+        title: _.kebabCase(this.props.title),
+        content: this.props.content,
+        trigger: this.props.trigger,
+        scope: this.props.scope
+      }
     );
-  },
-
-  install: function () {
-    this.installDependencies();
   }
 });
